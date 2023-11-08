8d14c9c2057e28adb574dac83893d87d279865b5
mwyzer <muhammad.wyzer@gmail.com>
2023-11-08 21:52:42 +0700

    get login user profile

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
-
-// @route       GET api/profile
-// @desc        Test route
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+// @route       GET api/profile/me
+// @desc        Get current users profile
 // @access      Public
-router.get("/", (req, res) => res.send("Profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ message: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
