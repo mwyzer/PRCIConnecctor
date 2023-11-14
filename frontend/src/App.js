4a08784f70ff49d899734f6a84517c1ffaea4267
mwyzer <muhammad.wyzer@gmail.com>
2023-11-14 20:47:04 +0700

    auth reducer & register action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Alert />
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
